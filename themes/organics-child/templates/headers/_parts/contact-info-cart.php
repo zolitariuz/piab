ee66cc25ddaafeb6bd195f08b73a5798fa6947a1
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2017-07-09 17:44:40 -0500

    traducciones theme

--- themes/organics-child/templates/headers/_parts/contact-info-cart.php
+++ themes/organics-child/templates/headers/_parts/contact-info-cart.php
@@ -4,10 +4,10 @@ $cart_summa = strip_tags(WC()->cart->get_cart_subtotal());
 ?>
 <a href="#" class="top_panel_cart_button" data-items="<?php echo esc_attr($cart_items); ?>" data-summa="<?php echo esc_attr($cart_summa); ?>">
 	<span class="contact_icon icon-shopping-cart13"></span>
-	<span class="contact_label contact_cart_label"><?php esc_html_e('Your cart:', 'organics'); ?></span>
+	<span class="contact_label contact_cart_label"><?php esc_html_e('Tu carrito:', 'organics'); ?></span>
 	<span class="contact_cart_totals">
 		<span class="cart_items"><?php
-			echo esc_html($cart_items) . ' ' . ($cart_items == 1 ? esc_html__('Item', 'organics') : esc_html__('Items', 'organics'));
+			echo esc_html($cart_items) . ' ' . ($cart_items == 1 ? esc_html__('Producto', 'organics') : esc_html__('Productos', 'organics'));
 		?></span>
 		-
 		<span class="cart_summa"><?php echo trim($cart_summa); ?></span>
